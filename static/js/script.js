468678d2dd5298de39bdb047c33f2a8b01e2fd60
INYONG KIM <ohnyong87@gmail.com>
2023-07-18 10:55:11 +0900

    Back2_Commit 1
    응원 댓글 남기기 기능 POST 구현
    
    데이터 명세
    *DB : MongoDB
    *Collection : fan
    *Document :
    -닉네임 : 'name':'name_receive' / from Frontend #name-> formData 'name_give':'value'
    -응원 댓글 내용 : 'comment':'comment_receive' / from Frontend #comment-> formData 'comment_give':'value'
    
    DB 연결
    *pymongo 임포트 및 client atlas cloud 링크 설정
    *certifi 임포트
    
    app.py DB Insert(Create) 작성
    *@app.route("/guestbook", methods=["POST"]) 작성
    *request.form을 통해 ['key']에 해당되는 value를 가져오고 각 변수 "name_receive", "comment_receive"라는 변수에 담는다
    *doc이라는 객체 생성
    -'name' : name_receive
    -'comment' : comment_receive
    *위 객체는 fan라는 collection에 insert_one하는 부분을 작성
    *이후, 확인용으로 {'msg' : 'POST 연결 완료! + DB 저장(insert) 완료!'} 라 하드코딩된 key, value가
    *jsonify()에 의해 json 형식의 데이터로 변환(주어진 값과 대응하는 JSON 문자열) 후 반환하고 js로 이동

--- static/js/script.js
+++ static/js/script.js
@@ -6,12 +6,20 @@ $(document).ready(function () {
 });
 // [Create]
 function save_comment() {
+    // index.html로부터 값 가져오기
+    let name = $('#name').val()
+    let comment = $('#comment').val()
+    // formData 객체를 생성하고
     let formData = new FormData();
-    formData.append("sample_give", "샘플데이터");
-
+    // append()통해 {key,value}를 객체에 담는다
+    formData.append("name_give", name);
+    formData.append("comment_give", comment);
+    // POST 요청에 위 formData를 body에 담아 요청한다.
     fetch('/guestbook', { method: "POST", body: formData }).then((res) => res.json()).then((data) => {
         console.log(data)
-        alert(data["msg"]);
+        alert(data["msg"])
+        // 브라우저 새로고침 추가
+        window.location.reload();
     });
 }
 // [Comment Read]
